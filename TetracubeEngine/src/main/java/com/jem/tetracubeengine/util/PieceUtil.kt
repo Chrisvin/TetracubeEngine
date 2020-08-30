55596bc961005e5d7aa8de8208e278fb56319aef
Jem <chrisvin1997@gmail.com>
2020-08-30 22:57:37 +0530

    Add PieceUtil

---
+++ TetracubeEngine/src/main/java/com/jem/tetracubeengine/util/PieceUtil.kt
@@ -0,0 +1,22 @@
+package com.jem.tetracubeengine.util
+
+import com.jem.tetracubeengine.pieces.*
+import java.util.*
+
+object PieceUtil {
+
+    private val random: Random = Random()
+
+    fun getRandomPiece(): Piece {
+        // TODO: Change random implementation so that pieces don't repeat more than 2 times in a row?
+        return when (random.nextInt(7)) {
+            0 -> IPiece()
+            1 -> JPiece()
+            2 -> LPiece()
+            3 -> OPiece()
+            4 -> SPiece()
+            5 -> TPiece()
+            /*6*/ else -> ZPiece()
+        }
+    }
+}
