0fb4e392efa2639e53e7cf7ab2b021572da2d71f
Jem <chrisvin1997@gmail.com>
2020-08-30 23:24:40 +0530

    Make currentPiece non nullable in TEngine

--- TetracubeEngine/src/main/java/com/jem/tetracubeengine/TetracubeEngine.kt
+++ TetracubeEngine/src/main/java/com/jem/tetracubeengine/TetracubeEngine.kt
@@ -1,6 +1,7 @@
 package com.jem.tetracubeengine
 
 import com.jem.tetracubeengine.listener.GameStateListener
+import com.jem.tetracubeengine.pieces.OPiece
 import com.jem.tetracubeengine.pieces.Piece
 import com.jem.tetracubeengine.util.PieceUtil
 import java.util.*
@@ -20,7 +21,7 @@ class TetracubeEngine {
     /**
      * The piece that is currently controlled by the player & is being dropped in the board
      */
-    var currentPiece: Piece? = null
+    var currentPiece: Piece = OPiece()
         private set
 
     /**
@@ -59,40 +60,34 @@ class TetracubeEngine {
      */
     private val gameTask: TimerTask = object : TimerTask() {
         override fun run() {
-            currentPiece?.let {
-                board.undo()
-                previousY--
-                val placeStatus = board.placePiece(it, previousX, previousY, previousZ)
-                when (placeStatus) {
-                    TetracubeBoard.PlaceStatus.SUCCESS -> {
-                        // Was able to place piece successfully
-                        // Check if piece can be moved further down next time.
-                    }
-                    TetracubeBoard.PlaceStatus.LAYER_FILLED -> {
-                        // This shouldn't be occurring here.
-                        // TODO: Add timber logs to check why this case is arising
-                    }
-                    TetracubeBoard.PlaceStatus.OUT_OF_BOUNDS -> {
-                        if (previousY - (currentPiece?.height ?: 0) < 0) {
-                            // Current piece hit the bottom of the grid
-                            placeCurrentPieceAndCalculateScore()
-                            prepareNewPiece()
-                        } else {
-                            // Shouldn't occur, but if it does, then it's probably one of the following
-                            // 1. width/breadth for the grid was too small
-                            // 2. previousX/previousZ is exceeding width/breadth limits
-                            // TODO: Add timber log to check above cases
-                        }
-                    }
-                    TetracubeBoard.PlaceStatus.OVERLAP_WITH_EXISTING -> {
-                        // Current piece hit some other existing blocks on the grid
+            board.undo()
+            previousY--
+            when (board.placePiece(currentPiece, previousX, previousY, previousZ)) {
+                TetracubeBoard.PlaceStatus.SUCCESS -> {
+                    // Was able to place piece successfully
+                    // Check if piece can be moved further down next time.
+                }
+                TetracubeBoard.PlaceStatus.LAYER_FILLED -> {
+                    // This shouldn't be occurring here.
+                    // TODO: Add timber logs to check why this case is arising
+                }
+                TetracubeBoard.PlaceStatus.OUT_OF_BOUNDS -> {
+                    if (previousY - currentPiece.height < 0) {
+                        // Current piece hit the bottom of the grid
                         placeCurrentPieceAndCalculateScore()
                         prepareNewPiece()
+                    } else {
+                        // Shouldn't occur, but if it does, then it's probably one of the following
+                        // 1. width/breadth for the grid was too small
+                        // 2. previousX/previousZ is exceeding width/breadth limits
+                        // TODO: Add timber log to check above cases
                     }
                 }
-            } ?: run {
-                // There's no reason this should ever occur
-                // TODO: Add Timber logs to check why current piece is null
+                TetracubeBoard.PlaceStatus.OVERLAP_WITH_EXISTING -> {
+                    // Current piece hit some other existing blocks on the grid
+                    placeCurrentPieceAndCalculateScore()
+                    prepareNewPiece()
+                }
             }
         }
     }
@@ -109,26 +104,24 @@ class TetracubeEngine {
 
     private fun placeCurrentPieceAndCalculateScore() {
         board.undo()
-        currentPiece?.let {
-            val placeStatus =
-                board.placePiece(it, previousX, previousY, previousZ + 1, checkLayerFilled = true)
-            when (placeStatus) {
-                TetracubeBoard.PlaceStatus.SUCCESS -> {
-                    // Successfully placed, but no layers where cleared, no score
-                }
-                TetracubeBoard.PlaceStatus.LAYER_FILLED -> {
-                    // Placing current piece causes 1 or more layers to be filled
-                    val clearedLayers = board.clearLayers()
-                    // TODO: Clear the rows and calculate the score
-                }
-                else -> {
-                    // This shouldn't occur, placing the piece should definitely be a success
-                    // TODO: Add timber log to check exactly what case this is & why it occurs
-                }
+        val placeStatus = board.placePiece(
+            currentPiece,
+            previousX, previousY, previousZ + 1,
+            checkLayerFilled = true
+        )
+        when (placeStatus) {
+            TetracubeBoard.PlaceStatus.SUCCESS -> {
+                // Successfully placed, but no layers where cleared, no score
             }
-        } ?: run {
-            // There's no reason this should ever occur
-            // TODO: Add Timber logs to check why current piece is null
+            TetracubeBoard.PlaceStatus.LAYER_FILLED -> {
+                // Placing current piece causes 1 or more layers to be filled
+                val clearedLayers = board.clearLayers()
+                // TODO: Clear the rows and calculate the score
+            }
+            else -> {
+                // This shouldn't occur, placing the piece should definitely be a success
+                // TODO: Add timber log to check exactly what case this is & why it occurs
+            }
         }
     }
 
