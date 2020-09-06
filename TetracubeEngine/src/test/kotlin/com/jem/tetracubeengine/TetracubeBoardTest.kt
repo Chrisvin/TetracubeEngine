d4cb1a38897365c789d7c50a0ae875055695b65a
Jem <chrisvin1997@gmail.com>
2020-09-07 00:44:10 +0530

    Update TBoard test cases

--- TetracubeEngine/src/test/kotlin/com/jem/tetracubeengine/TetracubeBoardTest.kt
+++ TetracubeEngine/src/test/kotlin/com/jem/tetracubeengine/TetracubeBoardTest.kt
@@ -1,7 +1,7 @@
 package com.jem.tetracubeengine
 
 import com.jem.tetracubeengine.pieces.*
-import junit.framework.Assert
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class TetracubeBoardTest {
@@ -12,34 +12,49 @@ class TetracubeBoardTest {
     @Test
     fun testDropHeight() {
         val board = TetracubeBoard()
-        board.placePiece(IPiece().apply { rotateClockwise() }, 5, 0, 0)
+        placePieceAndAssert(board, IPiece().apply { rotateClockwise() }, 5, 0, 0)
         for (piece in pieces) {
-            println(piece.pieceName)
-            Assert.assertEquals(
+            assertEquals(
                 4,
                 board.dropHeight(piece, 4, 0)
             )
         }
-        Assert.assertEquals(
+        assertEquals(
             4,
             board.dropHeight(IPiece().apply { rotateClockwise() }, 5, 0)
         )
-        board.placePiece(
+        placePieceAndAssert(
+            board,
             IPiece().apply { rotateClockwise() },
             5,
             board.dropHeight(IPiece().apply { rotateClockwise() }, 5, 0),
             0
         )
         for (piece in pieces) {
-            println(piece.pieceName)
-            Assert.assertEquals(
+            assertEquals(
                 8,
                 board.dropHeight(piece, 4, 0)
             )
         }
-        Assert.assertEquals(
+        assertEquals(
             8,
             board.dropHeight(IPiece().apply { rotateClockwise() }, 5, 0)
         )
+        placePieceAndAssert(board, IPiece(), 2, 8, 0)
+        for (piece in pieces) {
+            println(piece.pieceName)
+            assertEquals(
+                0,
+                board.dropHeight(piece, 1, 0, 4)
+            )
+            assertEquals(
+                0,
+                board.dropHeight(piece.apply { rotateCounterClockwise() }, 1, 0, 4)
+            )
+        }
+    }
+
+    private fun placePieceAndAssert(board: TetracubeBoard, piece: Piece, x: Int, y: Int, z: Int) {
+        assert(board.placePiece(piece, x, y, z) == TetracubeBoard.PlaceStatus.SUCCESS)
     }
 }
